678d754984fbc360fd4fe0595a1c4aeb2a265716
filizingithub <filizinusa@gmail.com>
2021-02-28 01:43:17 -0600

    Generate data from faker class on the registration page

--- src/test/java/gmibank/step_definitions/RegistrationStepDef.java
+++ src/test/java/gmibank/step_definitions/RegistrationStepDef.java
@@ -8,10 +8,9 @@ import gmibank.pages.HomePage;
 import gmibank.pages.RegistrationPage;
 import gmibank.pojos.Customer;
 import gmibank.pojos.User;
-import gmibank.utilities.ConfigurationReader;
-import gmibank.utilities.Driver;
-import gmibank.utilities.UsefulMethods;
+import gmibank.utilities.*;
 import org.junit.Assert;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
@@ -28,6 +27,7 @@ public class RegistrationStepDef {
     String lastName;
     String password;
     String username;
+    String ssn;
 
 
     @Given("User is on the home page")
@@ -44,137 +44,166 @@ public class RegistrationStepDef {
     @Given("user enters a  {int} digit SSN {string}")
     public void user_enters_a_digit_SSN(Integer int1, String ssn) {
 
+        ssn=faker.idNumber().ssnValid();
+        this.ssn=ssn;
         registrationPage.ssnTextbox.sendKeys(ssn);
+        customer.setSsn(ssn);
     }
 
     @When("user enters a name{string}")
     public void user_enters_a_name(String firstname) {
+
+        firstname=faker.name().firstName();
+        this.firstName=firstname;
+
         registrationPage.firstnameTextbox.sendKeys(firstname);
+        customer.setFirstName(firstName);
+
     }
 
     @When("user enter a lastname {string}")
+
     public void user_enter_a_lastname(String lastname) {
 
+        lastname=faker.name().lastName();
+        this.lastName=lastname;
         registrationPage.lastnameTextbox.sendKeys(lastname);
-
+        customer.setLastName(lastname);
     }
 
     @When("user enter an address {string} with zip")
     public void user_enter_an_address_with_zip(String address) {
-        registrationPage.addressTextbox.sendKeys(address);
+
+        address = faker.address().streetAddress();
+        Driver.waitAndSendText(registrationPage.addressTextbox,address,5);
+        customer.setAddress(address);
 
     }
 
     @When("user enter a {int} digits phone {string} number")
     public void user_enter_a_digits_phone_number(Integer int1, String phoneNumber) {
-
-        registrationPage.mobilephoneTextbox.sendKeys(phoneNumber);
+        boolean validPhone=false;
+        while(!validPhone){
+            phoneNumber = faker.phoneNumber().cellPhone();
+            validPhone=UsefulMethods.isValidPhoneNumber(phoneNumber);
+        }
+        Driver.waitAndSendText( registrationPage.mobilephoneTextbox,phoneNumber,5);
+        customer.setMobilePhoneNumber(phoneNumber);
 
     }
 
     @When("user create a username {string} without digits")
     public void user_create_a_username_without_digits(String username) {
-        registrationPage.usernameTextbox.sendKeys(username);
+        username = faker.name().username();
+        this.username = username;
+        Driver.waitAndSendText( registrationPage.usernameTextbox,username,5);
+
 
     }
 
     @When("user enter a email {string} address")
     public void user_enter_a_email_address(String email) {
-        registrationPage.emailTextbox.sendKeys(email);
+
+        email = firstName+lastName+"@gmail.com";
+        Driver.waitAndSendText(registrationPage.emailTextbox,email,5);
+
+        customer.setEmail(email);
 
     }
 
     @When("user enter a valid password {string}")
     public void user_enter_a_valid_password(String password) {
-        registrationPage.firstPasswordTextbox.sendKeys(password);
+
+        password = faker.internet().password(10,15,true,true);
+        this.password = password;
+        Driver.waitAndSendText( registrationPage.firstPasswordTextbox,password,5);
+
 
     }
 
     @When("user reenter the {string}")
-    public void user_reenter_the(String repeatPassword) {
-        registrationPage.newPasswordTextbox.sendKeys(repeatPassword);
-
+    public void user_reenter_the(String repeatedPassword) {
+        repeatedPassword=password;
+        Driver.waitAndSendText(registrationPage.newPasswordTextbox,repeatedPassword,3);
     }
 
     @When("click on the register")
     public void click_on_the_register() throws InterruptedException {
-
-        Thread.sleep(3000);
-        registrationPage.registerSubmitButton.click();
-        Thread.sleep(5000);
-    }
-
-    @Then("verify the invalid feedback")
-    public void verify_the_invalid_feedback() throws InterruptedException {
-        boolean verify = true;
-        List<WebElement> expectedFeedback = registrationPage.fieldList;
-        int size = UsefulMethods.getTextFromWebElements(expectedFeedback).size();
-        System.out.println("Size" + size);
-        verify = size > 0;
-        Assert.assertTrue(verify);
+        Driver.waitAndClick(registrationPage.registerSubmitButton,3);
+        WriteToTxt.saveAllCustomer(fileName, customer,password,username );
 
     }
-
-    @When("User enters invalid as ssn {string}")
-    public void userEntersInvalidAsSsn(String ssn) throws InterruptedException {
-
-        Thread.sleep(2000);
-        registrationPage.ssnTextbox.sendKeys(ssn);
-        Thread.sleep(2000);
-        registrationPage.registerSubmitButton.click();
-        Assert.assertFalse(UsefulMethods.isValidSSN(ssn));
-
-    }
-
-    @Then("System should display {string}")
-    public void system_should_display(String string) {
-
-        Assert.assertEquals(string, Driver.waitAndGetText(registrationPage.invalidSSnWarningMessage, 3));
-        System.out.println(registrationPage.invalidSSnWarningMessage);
-
-    }
-
-    @When("User enter invalid mobile {string}")
-    public void userEnterInvalidMobile(String mobilePhone) {
-
-        registrationPage.mobilephoneTextbox.sendKeys(mobilePhone);
-        registrationPage.registerSubmitButton.click();
-        Assert.assertFalse(UsefulMethods.isValidPhoneNumber(mobilePhone));
-
-    }
-
-    @Then("System should display this message {string}")
-    public void systemShouldDisplayThisMessage(String arg0) {
-
-        Assert.assertEquals(arg0, Driver.waitAndGetText(registrationPage.invalidPhoneNumberWarningMessage, 3));
-        System.out.println(registrationPage.invalidPhoneNumberWarningMessage);
-    }
-
-
-    @When("User enters invalid {string}")
-    public void userEntersInvalid(String email) {
-        registrationPage.emailTextbox.sendKeys(email);
-        registrationPage.registerSubmitButton.click();
-        Assert.assertFalse(UsefulMethods.isValidEmailAddress(email));
-    }
-
-    @Then("System should display email message {string}")
-    public void systemShouldDisplayEmailMessage(String arg0) {
-
-        Assert.assertEquals(arg0, Driver.waitAndGetText(registrationPage.invalidEmailWarningmessage, 1));
-        System.out.println(registrationPage.invalidEmailWarningmessage);
-    }
-
     @Then("verify the success message")
     public void verifyTheSuccessMessage() {
 
-        Driver.waitAndClick(registrationPage.registerSubmitButton, 6);
-        // String succesPopUp =Driver.getDriver().switchTo().alert().getText();
-        // System.out.println(succesPopUp);
-        String actual = registrationPage.successMessage.getText();
+        BrowserUtils.switchToWindow(registrationPage.successMessage.getText());
+        String actual= Driver.waitAndGetText(registrationPage.successMessage,10);
         Assert.assertEquals("Registration saved!", actual);
         System.out.println(actual);
 
 
     }
+//
+//    @Then("verify the invalid feedback")
+//    public void verify_the_invalid_feedback() throws InterruptedException {
+//        boolean verify = true;
+//        List<WebElement> expectedFeedback = registrationPage.fieldList;
+//        int size = UsefulMethods.getTextFromWebElements(expectedFeedback).size();
+//        System.out.println("Size" + size);
+//        verify = size > 0;
+//        Assert.assertTrue(verify);
+//
+//    }
+//
+//    @When("User enters invalid as ssn {string}")
+//    public void userEntersInvalidAsSsn(String ssn) throws InterruptedException {
+//
+//        Thread.sleep(2000);
+//        registrationPage.ssnTextbox.sendKeys(ssn);
+//        Thread.sleep(2000);
+//        registrationPage.registerSubmitButton.click();
+//        Assert.assertFalse(UsefulMethods.isValidSSN(ssn));
+//
+//    }
+//
+//    @Then("System should display {string}")
+//    public void system_should_display(String string) {
+//
+//        Assert.assertEquals(string, Driver.waitAndGetText(registrationPage.invalidSSnWarningMessage, 3));
+//        System.out.println(registrationPage.invalidSSnWarningMessage);
+//
+//    }
+//
+//    @When("User enter invalid mobile {string}")
+//    public void userEnterInvalidMobile(String mobilePhone) {
+//
+//        registrationPage.mobilephoneTextbox.sendKeys(mobilePhone);
+//        registrationPage.registerSubmitButton.click();
+//        Assert.assertFalse(UsefulMethods.isValidPhoneNumber(mobilePhone));
+//
+//    }
+//
+//    @Then("System should display this message {string}")
+//    public void systemShouldDisplayThisMessage(String arg0) {
+//
+//        Assert.assertEquals(arg0, Driver.waitAndGetText(registrationPage.invalidPhoneNumberWarningMessage, 3));
+//        System.out.println(registrationPage.invalidPhoneNumberWarningMessage);
+//    }
+//
+//
+//    @When("User enters invalid {string}")
+//    public void userEntersInvalid(String email) {
+//        registrationPage.emailTextbox.sendKeys(email);
+//        registrationPage.registerSubmitButton.click();
+//        Assert.assertFalse(UsefulMethods.isValidEmailAddress(email));
+//    }
+//
+//    @Then("System should display email message {string}")
+//    public void systemShouldDisplayEmailMessage(String arg0) {
+//
+//        Assert.assertEquals(arg0, Driver.waitAndGetText(registrationPage.invalidEmailWarningmessage, 1));
+//        System.out.println(registrationPage.invalidEmailWarningmessage);
+//    }
+
+
 }
--- target/test-classes/features/RegistrationPage.feature
+++ target/test-classes/features/RegistrationPage.feature
@@ -7,80 +7,76 @@ Feature:Registration page feature
     And User navigates to registration page
 
   @ValidCredentials
-  Scenario Outline: Register with valid credentials
-    Given user enters a  9 digit SSN "<SSN>"
-    When user enters a name" <name>"
-    When user enter a lastname "<lastname>"
-    When user enter an address "<address>" with zip
-    When user enter a 10 digits phone "<phone>" number
-    When user create a username "<username>" without digits
-    When user enter a email "<email>" address
-    When user enter a valid password "<password>"
-    When user reenter the "<confirmation password>"
+  Scenario:Register with valid credentials
+    Given user enters a  9 digit SSN "SSN"
+    When user enters a name"name"
+    When user enter a lastname "lastname"
+    When user enter an address "address" with zip
+    When user enter a 10 digits phone "phone" number
+    When user create a username "username" without digits
+    When user enter a email "email" address
+    When user enter a valid password "password"
+    When user reenter the "confirmation password"
     When  click on the register
     Then verify the success message
-    Examples:
-      |SSN        |name    |lastname|address             |phone       |username|email         |password|confirmation password|
-#      |423-45-6719|Patrick |Jane    |123 WALLSTREET 78762|832-654-3210|Demo12  |demo@gmail.com|1234As  |1234As               |
-      |760-45-6719|Walter |Bishop    |45 Science Ln 78562|832-654-3210|Genius  |bishop@gmail.com|1234As$  |1234As$               |
 
-
-  @InvalidCredentials
-  Scenario Outline: Register with invalid credentials
-    Given user enters a  9 digit SSN "<SSN>"
-    When user enters a name" <name>"
-    When user enter a lastname "<lastname>"
-    When user enter an address "<address>" with zip
-    When user enter a 10 digits phone "<phone>" number
-    When user create a username "<username>" without digits
-    When user enter a email "<email>" address
-    When user enter a valid password "<password>"
-    When user reenter the "<confirmation password>"
-    When  click on the register
-    Then verify the invalid feedback
-    Examples:
-      |SSN|name|lastname|address|phone|username|email|password|confirmation password|
-      |   |     |       |       |     |        |      |       |                     |
-      |343-78-9345|Filiz|   |Turkey|123-385-2354|User|usertestr@gmail.com|Abcds!|Abcds!|
-      |343-78-9345|Filiz|   |Turkey|123-385-2354|User|usertestr@gmail.com|      |Abcds!|
-     # |343-78-9345|Filiz|Abcde |      |      |User|usertestr@gmail.com|Abcds!|Abcds!|  Buggg
-
-
-   @InvalidSSN
-   Scenario Outline: SSN number cannot be of any chars, spec chars nor "0" and "9" except "-"
-     When User enters invalid as ssn "<SSN>"
-     Then System should display "Your SSN is invalid"
-     Examples:
-     |  SSN        |
-     |999-75-7654  |
-     |000-76-5670  |
-     |abc-65-7652  |
-     |765987897    |
-     |123-44-444444|
-  @InvalidPhoneNumber
-  Scenario Outline: Mobile phone number cannot be of any chars nor spec chars except "-"
-    When User enter invalid mobile "<phone number>"
-    Then System should display this message "Your mobile phone number is invalid"
-    Examples:
-    |phone number     |
-    |832555-5555      |
-    |abc-cde-efgh     |
-   # |832-555-55555555 | Bug
-    |83$-555-5555     |
-   # |000-000-0000     | Bug
-
-  @InvalidEmail
-  Scenario Outline:  email id cannot be created without "@" sign and "." extensions
-    When User enters invalid "<emailFormat>"
-    Then System should display email message "This field is invalid"
-    Examples:
-     |  emailFormat         |
-     | testuser@.gmail.com  |
-     | testusergmail.com    |
-     | testuser@gmailcom    |
-     | testuser@gmail.com.  |
-     #| testuser@gmail.3    |??
-     | .testuser@gmail.com  |
+#
+#  @InvalidCredentials
+#  Scenario Outline: Register with invalid credentials
+#    Given user enters a  9 digit SSN "<SSN>"
+#    When user enters a name" <name>"
+#    When user enter a lastname "<lastname>"
+#    When user enter an address "<address>" with zip
+#    When user enter a 10 digits phone "<phone>" number
+#    When user create a username "<username>" without digits
+#    When user enter a email "<email>" address
+#    When user enter a valid password "<password>"
+#    When user reenter the "<confirmation password>"
+#    When  click on the register
+#    Then verify the invalid feedback
+#    Examples:
+#      |SSN|name|lastname|address|phone|username|email|password|confirmation password|
+#      |   |     |       |       |     |        |      |       |                     |
+#      |343-78-9345|Filiz|   |Turkey|123-385-2354|User|usertestr@gmail.com|Abcds!|Abcds!|
+#      |343-78-9345|Filiz|   |Turkey|123-385-2354|User|usertestr@gmail.com|      |Abcds!|
+#     # |343-78-9345|Filiz|Abcde |      |      |User|usertestr@gmail.com|Abcds!|Abcds!|  Buggg
+#
+#
+#   @InvalidSSN
+#   Scenario Outline: SSN number cannot be of any chars, spec chars nor "0" and "9" except "-"
+#     When User enters invalid as ssn "<SSN>"
+#     Then System should display "Your SSN is invalid"
+#     Examples:
+#     |  SSN        |
+#     |999-75-7654  |
+#     |000-76-5670  |
+#     |abc-65-7652  |
+#     |765987897    |
+#     |123-44-444444|
+#  @InvalidPhoneNumber
+#  Scenario Outline: Mobile phone number cannot be of any chars nor spec chars except "-"
+#    When User enter invalid mobile "<phone number>"
+#    Then System should display this message "Your mobile phone number is invalid"
+#    Examples:
+#    |phone number     |
+#    |832555-5555      |
+#    |abc-cde-efgh     |
+#   # |832-555-55555555 | Bug
+#    |83$-555-5555     |
+#   # |000-000-0000     | Bug
+#
+#  @InvalidEmail
+#  Scenario Outline:  email id cannot be created without "@" sign and "." extensions
+#    When User enters invalid "<emailFormat>"
+#    Then System should display email message "This field is invalid"
+#    Examples:
+#     |  emailFormat         |
+#     | testuser@.gmail.com  |
+#     | testusergmail.com    |
+#     | testuser@gmailcom    |
+#     | testuser@gmail.com.  |
+#     #| testuser@gmail.3    |??
+#     | .testuser@gmail.com  |
 
 
 
